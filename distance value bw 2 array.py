b0170cd2dfbeb37b162ec40bb0d9037b65750438
Srikanth <154430800+SrikanthSrinivasan2003@users.noreply.github.com>
2024-06-03 10:21:40 +0530

    Distance value between 2 array.py

--- distance value bw 2 array.py
+++ distance value bw 2 array.py
@@ -4,22 +4,18 @@ d = int(input("Enter distance value:\n"))
 n_array = []
 m_array = []
 count = 0
-
 for i in range(n):
     val = int(input("Enter first array Elements:\n"))
     n_array += [val]
-
 for i in range(m):
     val = int(input("Enter second array Elements:\n"))
     m_array += [val]
-
 for i in range(n):
     for j in range(m):
         value = n_array[i] - m_array[j]
         if value < 0:
             value *= -1
-            if value <= d:
-                count += 1
-
+        if value < d:
+            count += 1
 print(count)
 
